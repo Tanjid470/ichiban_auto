a41a716bd03ccc4ef16abe5bbe0356d810e0e10a
Tanjid470 <tanjidamran11@gmail.com>
2024-10-21 16:28:00 +0600

    main build used GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:get/get.dart';
+import 'package:ichiban_auto/const/app_color.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+import 'backend/google_sheet_init.dart';
+import 'const/dynamic_font.dart';
 import 'module/splash/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await GoogleSheetInit().inti();
   runApp(const MyApp());
 }
 
@@ -15,8 +22,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: SplashScreen());
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const SplashScreen(),
+        builder: FlutterSmartDialog.init(
+          toastBuilder: (String msg) => Center(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 8),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+                  color: Colors.red.shade300
+              ),
+              child: Text(msg,
+                style:   TextStyle(
+                  color: Colors.white,
+                  fontSize: TextSize.font16(context),
+                ),
+              ),
+            ),
+          ),
+          loadingBuilder: (String msg) => LoadingAnimationWidget.hexagonDots(
+              color:AppColors.baseColorRed,
+              size: 100
+          ),
+        ),
+      );
     }
 }
 
