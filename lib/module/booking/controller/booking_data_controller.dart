89b12e1f1deedc96154fbbb95c8eb2b0566c916c
Tanjid470 <tanjidamran11@gmail.com>
2024-10-21 16:28:49 +0600

    here keep the all text field data into textEditingController

---
+++ lib/module/booking/controller/booking_data_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BookingDataController extends GetxController{
+  TextEditingController carMakeController = TextEditingController();
+  TextEditingController carModelController = TextEditingController();
+  TextEditingController carYearController = TextEditingController();
+  TextEditingController registrationPlateController = TextEditingController();
+  TextEditingController customerNameController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController bookingTitleController = TextEditingController();
+
+}
