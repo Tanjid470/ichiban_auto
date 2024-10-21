d01993ab57846d81f9a7f4d172eb538aa548f1ab
Tanjid470 <tanjidamran11@gmail.com>
2024-10-21 09:22:47 +0600

    have a simple bug signup method

---
+++ lib/module/auth/service/firebase_auth.dart
@@ -0,0 +1,49 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+
+class AuthServices {
+  static signupUser(String email, String password, String name, BuildContext context) async {
+    try {
+
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Registration Successful')));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Password Provided is too weak')));
+      } else if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Email Provided already Exists')));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  static signinUser(String email, String password, BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('You are Logged in')));
+    } on FirebaseAuthException catch (exception) {
+      if (exception.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('No user Found with this Email')));
+      } else if (exception.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Password did not match')));
+      } else if (exception.code == 'invalid-credential') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('The supplied credential is invalid or has expired')));
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("${exception.message}")));
+      }
+    }
+  }
+
+}
