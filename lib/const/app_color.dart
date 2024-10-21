9be75fb3a19f508653d58b495b4657787f762f44
Tanjid470 <tanjidamran11@gmail.com>
2024-10-21 10:19:15 +0600

    ichiban auto theme color added

--- lib/const/app_color.dart
+++ lib/const/app_color.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
-  static Color themeColor = Colors.red;
-  static  Color themeColorShade200 = Colors.red.shade200;
+  static Color baseColorRed = const Color(0xffd71f26);
+  static Color baseColorGrey = const Color(0xff808282);
+  static  Color themeColorShade200 = baseColorRed.withOpacity(.5);
   static Gradient bgGradient = const LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
