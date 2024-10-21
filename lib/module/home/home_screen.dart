a1c33fb312bf9a51f55590eb642804ef6abaca4a
Tanjid470 <tanjidamran11@gmail.com>
2024-10-21 13:03:33 +0600

    add card for booking by admin and calender view

--- lib/module/home/home_screen.dart
+++ lib/module/home/home_screen.dart
@@ -1,7 +1,11 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ichiban_auto/config/responsive_scale.dart';
 import 'package:ichiban_auto/const/app_color.dart';
+
+import '../../const/dynamic_font.dart';
+import '../booking/car_service_booking.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -28,7 +32,67 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Dashboard'),
       ),
       body: Container(
-        color: AppColors.themeColor,
+        padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Booking for Car Servicing',style: TextStyle(color: AppColors.baseColorRed,fontSize: TextSize.font20(context),fontWeight: FontWeight.w700),),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CarServiceForm()),
+                );
+
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width-30,
+                height: MediaQuery.of(context).size.height/4,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.baseColorRed,
+                        //spreadRadius: 2,
+                        blurRadius: 2,
+                        blurStyle: BlurStyle.outer
+                      )
+                    ],
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/car_service.png'),
+                      fit: BoxFit.fitHeight
+                    )
+                ),
+              ),
+            ),
+            ResponsiveScale.of(context).verticalGap(context, 2),
+            Text('Calendar View of Bookings',style: TextStyle(color: AppColors.baseColorRed,fontSize: TextSize.font20(context),fontWeight: FontWeight.w700),),
+            Container(
+              width: MediaQuery.of(context).size.width-30,
+              height: MediaQuery.of(context).size.height/4,
+
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.baseColorRed,
+                      //spreadRadius: 2,
+                      blurRadius: 2,
+                      blurStyle: BlurStyle.outer
+                    )
+                  ],
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/car_admin.png'),
+                    fit: BoxFit.fitHeight
+                  )
+              ),
+            ),
+          ],
+        ),
+
       ),
     );
   }
