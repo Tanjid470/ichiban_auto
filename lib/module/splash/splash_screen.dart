481a0b2f4019cf089aae5f8add6f4eacaf1d07c7
Tanjid470 <tanjidamran11@gmail.com>
2024-10-21 09:25:24 +0600

    normal splash

---
+++ lib/module/splash/splash_screen.dart
@@ -0,0 +1,46 @@
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ichiban_auto/const/app_color.dart';
+import 'package:ichiban_auto/module/auth/view/login_form.dart';
+import 'package:ichiban_auto/module/home/home_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: AnimatedSplashScreen(
+          splash:const Text(".",style: TextStyle(color: Colors.white,fontSize:100 ),),
+
+          splashIconSize: 100,
+          duration: 100,
+          animationDuration: Duration(seconds: 2),
+          splashTransition: SplashTransition.rotationTransition,
+          backgroundColor: AppColors.themeColorShade200,
+          nextScreen: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return const HomeScreen();
+              } else {
+                return const LoginForm();
+              }
+            },
+          ),
+        )
+    );
+  }
+}
