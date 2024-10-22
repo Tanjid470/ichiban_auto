b10493d9df535606fdbd76ca64a62a21ae7f41dc
Tanjid470 <tanjidamran11@gmail.com>
2024-10-22 16:33:21 +0600

    calender view page route

--- lib/module/home/view/home_screen.dart
+++ lib/module/home/view/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:ichiban_auto/config/responsive_scale.dart';
 import 'package:ichiban_auto/const/app_color.dart';
 import 'package:ichiban_auto/const/app_text_style.dart';
+import 'package:ichiban_auto/module/calendar/view/calendar_view.dart';
 
 import '../../../const/dynamic_font.dart';
 import '../../booking/view/car_service_booking_screen.dart';
@@ -33,7 +34,6 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
         title:  Text('Dashboard',
           style: customSize(TextSize.font24(context),AppColors.baseColorRed),
-
         ),
         centerTitle: true,
         titleSpacing: 10,
@@ -51,7 +51,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   context,
                   MaterialPageRoute(builder: (context) =>const CarServiceForm()),
                 );
-
               },
               child: Container(
                 width: MediaQuery.of(context).size.width-30,
@@ -76,25 +75,33 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             ResponsiveScale.of(context).verticalGap(context, 2),
             Text('Calendar View of Bookings',style: TextStyle(color: AppColors.baseColorRed,fontSize: TextSize.font20(context),fontWeight: FontWeight.w700),),
-            Container(
-              width: MediaQuery.of(context).size.width-30,
-              height: MediaQuery.of(context).size.height/4,
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CalendarView()),
+                );
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width-30,
+                height: MediaQuery.of(context).size.height/4,
 
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: AppColors.baseColorRed,
-                      //spreadRadius: 2,
-                      blurRadius: 2,
-                      blurStyle: BlurStyle.outer
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.baseColorRed,
+                        //spreadRadius: 2,
+                        blurRadius: 2,
+                        blurStyle: BlurStyle.outer
+                      )
+                    ],
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/car_admin.png'),
+                      fit: BoxFit.fitHeight
                     )
-                  ],
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/car_admin.png'),
-                    fit: BoxFit.fitHeight
-                  )
+                ),
               ),
             ),
           ],
