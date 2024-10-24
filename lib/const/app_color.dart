b642a16099dcbb90dcb14f2af48cb17cad06a03f
Tanjid470 <tanjidamran11@gmail.com>
2024-10-24 11:16:19 +0600

    add 2 more color with less opacity

--- lib/const/app_color.dart
+++ lib/const/app_color.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
   static Color baseColorRed = const Color(0xffd71f26);
+  static Color baseColorRedShade100 = const Color(0xffd71f26).withOpacity(.1);
   static Color baseColorGrey = const Color(0xff808282);
-  static  Color themeColorShade200 = baseColorRed.withOpacity(.5);
+  static Color baseColorGreyShade100 = const Color(0xff808282);
   static Gradient bgGradient = const LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
