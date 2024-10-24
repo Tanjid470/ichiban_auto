419447e0df773dd416a00f415a0fd89de9e174d7
Tanjid470 <tanjidamran11@gmail.com>
2024-10-24 11:41:59 +0600

    splash screen bg color change and animation change

--- lib/module/splash/splash_screen.dart
+++ lib/module/splash/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ichiban_auto/const/app_color.dart';
+import 'package:ichiban_auto/const/dynamic_font.dart';
 import 'package:ichiban_auto/module/auth/view/login_form.dart';
 import 'package:ichiban_auto/module/home/view/home_screen.dart';
 
@@ -24,13 +25,19 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: AnimatedSplashScreen(
-          splash:const Text(".",style: TextStyle(color: Colors.white,fontSize:100 ),),
+          splash: Text("Ichiban Auto",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize:TextSize.font30(context),
+                fontWeight: FontWeight.w500 ),
+
+          ),
 
           splashIconSize: 100,
           duration: 100,
-          animationDuration: Duration(seconds: 2),
-          splashTransition: SplashTransition.rotationTransition,
-          backgroundColor: AppColors.baseColorRedShade100,
+          animationDuration: const Duration(seconds: 3),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor:AppColors.baseColorRed.withOpacity(.8),
           nextScreen: StreamBuilder(
             stream: FirebaseAuth.instance.authStateChanges(),
             builder: (context, snapshot) {
