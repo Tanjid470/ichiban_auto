89699dccf70f60790c30c585c9ee02c489275f38
Tanjid470 <tanjidamran11@gmail.com>
2024-10-24 11:15:35 +0600

    now it's show from a card

--- lib/module/calendar/view/date_wise_booking_view.dart
+++ lib/module/calendar/view/date_wise_booking_view.dart
@@ -5,6 +5,7 @@ import 'package:ichiban_auto/backend/car_booking_data_get.dart';
 import 'package:ichiban_auto/const/app_color.dart';
 import 'package:ichiban_auto/const/app_text_style.dart';
 import 'package:ichiban_auto/const/dynamic_font.dart';
+import 'package:ichiban_auto/global/booking_details_card.dart';
 import 'package:ichiban_auto/module/calendar/controller/data_get_view_controller.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
@@ -27,39 +28,30 @@ class _CalenderWiseViewState extends State<CalenderWiseView> {
   void fetchStudentData() async {
     await CarBookingDataGet().initAndFetchData(DateTime.now());
   }
+
   @override
   Widget build(BuildContext context) {
     return Obx(() => dataGetViewController.isLoading.value
         ? Center(
-      heightFactor: 10,
-      child: LoadingAnimationWidget.hexagonDots(
-          color: AppColors.baseColorRed,
-          size: Get.height/10
-      ),
-    )
+          heightFactor: 10,
+          child: LoadingAnimationWidget.hexagonDots(
+              color: AppColors.baseColorRed, size: Get.height / 10),
+        )
         : CarBookingDataGet.carBookingDataGet.isNotEmpty
           ? ListView.builder(
             shrinkWrap: true,
+            padding: const EdgeInsets.symmetric(vertical: 5),
             itemCount: CarBookingDataGet.carBookingDataGet.length,
             itemBuilder: (context, index) {
-              return Card(child: Column(
-                children: [
-                  Text(CarBookingDataGet.carBookingDataGet[index].brand.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].model.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].year.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].registration.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].customerName.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].customerNumber.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].customerEmail.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].bookingTitle.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].startDate.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].endDate.toString()),
-                  Text(CarBookingDataGet.carBookingDataGet[index].assignMechanic.toString()),
-                ],
-              ));
-            },)
-          :  Text("No deadline for today",
-                style: customSize(TextSize.font22(context),AppColors.baseColorRed))
-        );
+              return BookingDetailsCard(index: index,);
+            },
+          )
+          : Center(
+            child: Text("No deadline for this day",
+            style: customSize(TextSize.font22(context), AppColors.baseColorGrey)
+            ),
+          )
+    );
   }
 }
+
